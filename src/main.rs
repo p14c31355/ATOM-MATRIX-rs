7f04760e7b7d394e0c8e0f8440151b562727afd0
Placeless <thinkoverheat@gmail.com>
2025-09-05 10:28:46 +0900

    Grove ok

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,23 @@ fn main() -> anyhow::Result<()> {
 
     // --- I2C 初期化 ---
     let peripherals = Peripherals::take().unwrap();
-    let config = I2cConfig::default();
-    let mut i2c = I2cDriver::new(peripherals.i2c0, peripherals.pins.gpio21, peripherals.pins.gpio22, &config)?;
+    let config = I2cConfig::new().baudrate(esp_idf_hal::prelude::Hertz(100000));
+    let mut i2c = I2cDriver::new(peripherals.i2c0, peripherals.pins.gpio26, peripherals.pins.gpio32, &config)?;
+
+//     for addr in 0x00..=0x7F {
+//     if i2c.write(addr, &[0], 1000).is_ok() {
+//         println!("Found device @ 0x{:02X}", addr);
+//     }
+// }
 
     // --- SH1107G 初期化 ---
     let mut display = Sh1107gBuilder::new(&mut i2c)
         // .with_address(0x3C)
-        // .clear_on_init(true)
+        .clear_on_init(true)
         .build();
 
     display.init().unwrap();
+    display.clear_buffer();
 
     // --- 描画 ---
     Rectangle::new(Point::new(10, 10), Size::new(100, 50))
